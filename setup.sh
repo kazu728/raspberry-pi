5f421c05a054a6037884475080c4d23528ca5979
Kazuki Matsuo <kazuki.matsuo.728@gmail.com>
2021-05-25 02:14:29 +0900

    chore:updated setup

--- setup.sh
+++ setup.sh
@@ -1,12 +1,40 @@
 #!/bin/sh
 
-sudo apt update -y
+sudo su - 
+apt update -y && apt upgrade -y
 
-sudo apt install -y vim \
+install(){
+  apt install -y \
+  vim \
   sysstat \
   net-tools \
   wireshark \
-  namp \
-  httpd-tools \
+  nmap \
   docker.io \
-  docker-compose
+  docker-compose \
+  rkhunter \
+  build-essential
+}
+
+set_firewall(){
+
+  ufw enable 
+  ufw default deny
+
+  allowed_ports=(22 80 443)
+
+  for port in $allowed_ports
+  do
+    ufw allow $port
+  done
+}
+
+enable_monitoring(){
+  cd /etc/default/sysstat
+  sed -e "s/false/true"
+}
+
+install
+set_firewall
+enable_monitoring
+rkhunter -c
